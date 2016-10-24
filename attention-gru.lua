69e6e7ab1b6058dfe575f6f27531abe5a4b12ad6
Sean Robertson <sprobertson@gmail.com>
2016-10-24 12:32:52 -0700

    more comments

--- attention-gru.lua
+++ attention-gru.lua
@@ -3,23 +3,27 @@
 -- weighted sum of those states
 
 function AttentionGRU(input_size, hidden_size, max_length)
+    -- Inputs
     local input = nn.View(-1)()
     local prev_h = nn.View(-1)()
     local encs = nn.SplitTable(1)()
     local inputs = {input, prev_h, encs}
 
+    -- Calculate attention
     local encs_sum = nn.View(-1)(nn.CAddTable()(encs))
     local full_context = nn.JoinTable(1)({input, prev_h, encs_sum})
-    local attn_coef = nn.SoftMax()(nn.Linear(input_size + hidden_size * 2, max_length)(full_context))
+    local attn_context = nn.Linear(input_size + hidden_size * 2, max_length)(full_context)
+    local attn_coef = nn.SoftMax()(attn_context)
     local attn = nn.MixtureTable()({attn_coef, encs})
 
     function makeGate(i, h)
         local i2h = nn.Linear(input_size, hidden_size)(i)
         local h2h = nn.Linear(hidden_size, hidden_size)(h)
-        local a2h = nn.Linear(hidden_size, hidden_size)(attn)
+        local a2h = nn.Linear(hidden_size, hidden_size)(attn) -- Adds attention output to all gates
         return nn.CAddTable()({i2h, h2h, a2h})
     end
 
+    -- Regular GRU computation
     local z = nn.Sigmoid()(makeGate(input, prev_h))
     local r = nn.Sigmoid()(makeGate(input, prev_h))
 
@@ -31,6 +35,7 @@ function AttentionGRU(input_size, hidden_size, max_length)
     local nzh = nn.CMulTable()({nz, prev_h})
     local next_h = nn.CAddTable()({zh, nzh})
 
+    -- Output attention alongside state for later analysis
     local outputs = {next_h, attn_coef}
     return nn.gModule(inputs, outputs)
 end
